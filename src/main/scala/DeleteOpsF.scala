3db9784a6d98ad361094ceed1af0789a3dca7348
eqk <krylov_dan@mail.ru>
2021-08-12 11:32:23 +0300

    Insert ops + refactoring

--- src/main/scala/DeleteOpsF.scala
+++ src/main/scala/DeleteOpsF.scala
@@ -4,71 +4,28 @@ import scala.concurrent.ExecutionContext
 
 import cats.effect.Async
 import helpers._
-import reactivemongo.api.Collation
-import reactivemongo.api.bson.BSONDocumentWriter
-import reactivemongo.api.bson.collection.BSONCollection
+import reactivemongo.api.collections.GenericCollection
 import reactivemongo.api.commands.WriteResult
+import reactivemongo.api.{Collation, SerializationPack}
 
-object DeleteOpsF {
-  /*
-  def foo(col: BSONCollection) =
-    col.delete
 
-    (...).cursor.whaterF
-                ^ here is extra F versions of method
+case class DeleteOpsF[F[_] : Async, P <: SerializationPack](collection: GenericCollection[P]) {
+  def one[Q: collection.pack.Writer](
+    q: Q,
+    limit: Option[Int] = None,
+    collation: Option[Collation] = None
+  )(implicit ec: ExecutionContext): F[WriteResult] =
+    Async[F].fromFutureDelay(collection.delete.one(q, limit, collation))
 
-                (...).deleteF.whatever
-                (...).delete.(...)
-                    ^ here is extra delete F
+  def many(
+    deletes: Iterable[collection.DeleteElement]
+  )(implicit ec: ExecutionContext): F[collection.MultiBulkWriteResult] =
+    Async[F].fromFutureDelay(collection.delete.many(deletes))
 
-  def fooF[F[_]: Async](col: BSONCollection) =
-    col.deleteF.oneF()
-   */
-
-  implicit final class DeleteOpsFImpl(val delete: BSONCollection#DeleteBuilder) {
-    def oneF[F[_]: Async, Q: BSONDocumentWriter](
-        q: Q,
-        limit: Option[Int] = None,
-        collation: Option[Collation] = None
-    )(implicit ec: ExecutionContext): F[WriteResult] =
-      Async[F].fromFutureDelay(delete.one(q, limit, collation))
-
-    def manyF[F[_]: Async](
-        deletes: Iterable[BSONCollection#DeleteElement]
-    )(implicit ec: ExecutionContext): F[BSONCollection#MultiBulkWriteResult] =
-      ??? //Async[F].fromFutureDelay(delete.many(deletes))
-
-    def elementF[F[_]: Async, Q: BSONDocumentWriter](
-        q: Q,
-        limit: Option[Int] = None,
-        collation: Option[Collation] = None
-    ): F[BSONCollection#DeleteElement] =
-      Async[F].fromFutureDelay(delete.element(q, limit, collation))
-  }
-
-  case class DeleteFWrapper[F[_]: Async](val collection: BSONCollection) {
-    def one[Q: BSONDocumentWriter](
-        q: Q,
-        limit: Option[Int] = None,
-        collation: Option[Collation] = None
-    )(implicit ec: ExecutionContext): F[WriteResult] =
-      Async[F].fromFutureDelay(collection.delete.one(q, limit, collation))
-
-    def many(
-        deletes: Iterable[collection.DeleteElement]
-    )(implicit ec: ExecutionContext): F[collection.MultiBulkWriteResult] =
-      Async[F].fromFutureDelay(collection.delete.many(deletes))
-
-    def element[Q: BSONDocumentWriter](
-        q: Q,
-        limit: Option[Int] = None,
-        collation: Option[Collation] = None
-    ): F[collection.DeleteElement] =
-      Async[F].fromFutureDelay(collection.delete.element(q, limit, collation))
-  }
-
-  implicit final class DeleteOpsFImpl3(val collection: BSONCollection) {
-    def deleteF[F[_]: Async]: DeleteFWrapper[F] =
-      DeleteFWrapper(collection)
-  }
+  def element[Q: collection.pack.Writer](
+    q: Q,
+    limit: Option[Int] = None,
+    collation: Option[Collation] = None
+  ): F[collection.DeleteElement] =
+    Async[F].fromFutureDelay(collection.delete.element(q, limit, collation))
 }
--- src/main/scala/UpdateOpsF.scala
+++ src/main/scala/UpdateOpsF.scala
@@ -3,34 +3,31 @@ package com.bcf
 import scala.concurrent.ExecutionContext
 
 import cats.effect.Async
-import reactivemongo.api.Collation
-import reactivemongo.api.bson.BSONDocumentWriter
-import reactivemongo.api.bson.collection.BSONCollection
 import helpers._
+import reactivemongo.api.collections.GenericCollection
+import reactivemongo.api.{Collation, SerializationPack}
 
-object UpdateOpsF {
-  implicit final class UpdateOpsFImpl(val update: BSONCollection#UpdateBuilder) extends AnyVal {
-    def elementF[F[_]: Async, Q: BSONDocumentWriter, U: BSONDocumentWriter](
-      q: Q,
-      u: U,
-      upsert: Boolean = false,
-      multi: Boolean = false,
-      collation: Option[Collation] = None
-    ): F[BSONCollection#UpdateElement] =
-      Async[F].fromFutureDelay(update.element(q, u, upsert, multi, collation))
+case class UpdateOpsF[F[_] : Async, P <: SerializationPack](collection: GenericCollection[P]) {
+  def elementF[Q: collection.pack.Writer, U: collection.pack.Writer](
+    q: Q,
+    u: U,
+    upsert: Boolean = false,
+    multi: Boolean = false,
+    collation: Option[Collation] = None
+  ): F[collection.UpdateElement] =
+    Async[F].fromFutureDelay(collection.update.element(q, u, upsert, multi, collation))
 
-    def oneF[F[_]: Async, Q: BSONDocumentWriter, U: BSONDocumentWriter](
-      q: Q,
-      u: U,
-      upsert: Boolean = false,
-      multi: Boolean = false,
-      collation: Option[Collation] = None
-    )(implicit ec: ExecutionContext): F[BSONCollection#UpdateWriteResult] =
-      Async[F].fromFutureDelay(update.one(q, u, upsert, multi, collation))
+  def oneF[Q: collection.pack.Writer, U: collection.pack.Writer](
+    q: Q,
+    u: U,
+    upsert: Boolean = false,
+    multi: Boolean = false,
+    collation: Option[Collation] = None
+  )(implicit ec: ExecutionContext): F[collection.UpdateWriteResult] =
+    Async[F].fromFutureDelay(collection.update.one(q, u, upsert, multi, collation))
 
-    def manyF[F[_]: Async](
-      updates: Iterable[BSONCollection#UpdateElement]
-    )(implicit ec: ExecutionContext): F[BSONCollection#MultiBulkWriteResult] =
-      Async[F].fromFutureDelay(update.many(updates))
-  }
+  def manyF(
+    updates: Iterable[collection.UpdateElement]
+  )(implicit ec: ExecutionContext): F[collection.MultiBulkWriteResult] =
+    Async[F].fromFutureDelay(collection.update.many(updates))
 }
