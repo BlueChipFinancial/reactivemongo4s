c0057dbe47dfe3e8805cf7a08bd08f3c62cf1d39
ioleo <ioleo+git@protonmail.com>
2021-08-12 06:43:09 -0400

    CS

--- src/main/scala/InsertOpsF.scala
+++ src/main/scala/InsertOpsF.scala
@@ -8,15 +8,14 @@ import reactivemongo.api.SerializationPack
 import reactivemongo.api.collections.GenericCollection
 import reactivemongo.api.commands.WriteResult
 
-
-case class InsertOpsF[F[_] : Async, P <: SerializationPack](collection: GenericCollection[P]) {
+case class InsertOpsF[F[_]: Async, P <: SerializationPack](collection: GenericCollection[P]) {
   def one[T: collection.pack.Writer](
-    document: T
+      document: T
   )(implicit ec: ExecutionContext): F[WriteResult] =
     Async[F].fromFutureDelay(collection.insert.one(document))
 
   def many[T: collection.pack.Writer](
-    documents: Iterable[T]
+      documents: Iterable[T]
   )(implicit ec: ExecutionContext): F[collection.MultiBulkWriteResult] =
     Async[F].fromFutureDelay(collection.insert.many(documents))
 }
