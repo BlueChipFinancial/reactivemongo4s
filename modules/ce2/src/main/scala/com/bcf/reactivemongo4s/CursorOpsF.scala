fdd9544b1527579766b92094997ce4f4129ed7c8
eqk <krylov_dan@mail.ru>
2021-08-20 12:17:46 +0300

    toStream F[] removed

--- modules/ce2/src/main/scala/com/bcf/reactivemongo4s/CursorOpsF.scala
+++ modules/ce2/src/main/scala/com/bcf/reactivemongo4s/CursorOpsF.scala
@@ -4,7 +4,7 @@ import scala.collection.Factory
 import scala.concurrent.Future
 
 import cats.effect.concurrent.Deferred
-import cats.effect.{Async, Concurrent, ConcurrentEffect, Sync}
+import cats.effect.{Async, Concurrent, ConcurrentEffect}
 import cats.implicits._
 import com.bcf.reactivemongo4s.helpers._
 import fs2.concurrent.Queue
@@ -31,61 +31,57 @@ trait CursorOpsF {
     private def enqueueFuture[F[_]: ConcurrentEffect, A](queue: Queue[F, A], promise: Deferred[F, _])(el: A) =
       ConcurrentEffect[F].toIO(Concurrent[F].race(promise.get, queue.enqueue(Stream(el)).compile.drain)).unsafeToFuture()
 
-    def toStream[F[_]: ConcurrentEffect: MongoExecutor](queueCapacity: Int): F[Stream[F, T]] =
-      Sync[F].delay(
-        for {
-          queue <- Stream.eval(Queue.bounded[F, Option[Chunk[T]]](queueCapacity))
-          promise <- Stream.eval(Deferred[F, Unit])
-          _ <- Stream.bracket {
-            Async[F].fromFutureDelay { implicit ec =>
-              val enqueue = enqueueFuture[F, Option[Chunk[T]]](queue, promise) _
+    def toStream[F[_]: ConcurrentEffect: MongoExecutor](queueCapacity: Int): Stream[F, T] =
+      for {
+        queue <- Stream.eval(Queue.bounded[F, Option[Chunk[T]]](queueCapacity))
+        promise <- Stream.eval(Deferred[F, Unit])
+        _ <- Stream.bracket {
+          Async[F].fromFutureDelay { implicit ec =>
+            val enqueue = enqueueFuture[F, Option[Chunk[T]]](queue, promise) _
 
-              cursor
-                .foldBulksM(()) { (_, xs) =>
-                  val chunk = Chunk.seq(xs.toSeq)
-                  enqueue(chunk.some).flatMap {
+            cursor
+              .foldBulksM(()) { (_, xs) =>
+                val chunk = Chunk.seq(xs.toSeq)
+                enqueue(chunk.some).flatMap {
+                  case Left(_) =>
+                    enqueue(None).map(_ => Cursor.Done())
+                  case Right(_) =>
+                    Future.successful(Cursor.Cont())
+                }
+              }
+              .flatMap(_ => enqueue(None))
+
+            Future.successful(promise)
+          }
+        }(_.complete(()).void)
+        stream <- StreamHelpers.fromQueueNoneTerminatedChunk(queue)
+      } yield stream
+
+    def toStreamUnterminated[F[_]: ConcurrentEffect: MongoExecutor](capacity: Int): Stream[F, T] =
+      for {
+        queue <- Stream.eval(Queue.bounded[F, Chunk[T]](capacity))
+        promise <- Stream.eval(Deferred[F, Unit])
+        _ <- Stream.bracket {
+          Async[F].fromFutureDelay { implicit ec =>
+            val enqueue = enqueueFuture[F, Chunk[T]](queue, promise) _
+
+            cursor
+              .foldBulksM(()) { (_, xs) =>
+                val chunk = Chunk.seq(xs.toSeq)
+                if (chunk.isEmpty && cursor.tailable)
+                  Future.successful(Cursor.Cont(()))
+                else
+                  enqueue(chunk).flatMap {
                     case Left(_) =>
-                      enqueue(None).map(_ => Cursor.Done())
+                      Future.successful(Cursor.Done())
                     case Right(_) =>
                       Future.successful(Cursor.Cont())
                   }
-                }
-                .flatMap(_ => enqueue(None))
-
-              Future.successful(promise)
-            }
-          }(_.complete(()).void)
-          stream <- StreamHelpers.fromQueueNoneTerminatedChunk(queue)
-        } yield stream
-      )
-
-    def toStreamUnterminated[F[_]: ConcurrentEffect: MongoExecutor](capacity: Int): F[Stream[F, T]] =
-      Sync[F].delay(
-        for {
-          queue <- Stream.eval(Queue.bounded[F, Chunk[T]](capacity))
-          promise <- Stream.eval(Deferred[F, Unit])
-          _ <- Stream.bracket {
-            Async[F].fromFutureDelay { implicit ec =>
-              val enqueue = enqueueFuture[F, Chunk[T]](queue, promise) _
-
-              cursor
-                .foldBulksM(()) { (_, xs) =>
-                  val chunk = Chunk.seq(xs.toSeq)
-                  if (chunk.isEmpty && cursor.tailable)
-                    Future.successful(Cursor.Cont(()))
-                  else
-                    enqueue(chunk).flatMap {
-                      case Left(_) =>
-                        Future.successful(Cursor.Done())
-                      case Right(_) =>
-                        Future.successful(Cursor.Cont())
-                    }
-                }
-              Future.successful(promise)
-            }
-          }(_.complete(()).void)
-          stream <- StreamHelpers.fromQueueUnterminatedChunk(queue)
-        } yield stream
-      )
+              }
+            Future.successful(promise)
+          }
+        }(_.complete(()).void)
+        stream <- StreamHelpers.fromQueueUnterminatedChunk(queue)
+      } yield stream
   }
 }
